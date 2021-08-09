6485683f77cae0e8c8fc672a08740259c22568b3
sugizou0215@gmail.com <ec2-user@ip-172-31-48-89.ec2.internal>
2021-08-09 06:31:21 +0000

    [Finish]7-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ec6c160131ab7e3ac5605c59a7f923922257c98d0f172ed6cd7da1ee2aa73b3196c16e59c37bb8ea4e9b9f7d420bd5103b3ac55689dac65b751a0f98569360ec'
